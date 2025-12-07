c748289b3d55a107bdb5b0f09b60a429183d542b
Docker Build <docker@example.com>
2025-12-07 22:04:57 +0000

    fix: add strict xml schema to sitemap for validation

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -62,7 +62,7 @@ function generateSitemap() {
     console.log(`Found ${shops.length} shops.`);
 
     let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n';
 
     // Add static routes
     staticRoutes.forEach(route => {
